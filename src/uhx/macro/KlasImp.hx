5fd64c152a82f1e41ae77d46d91328c90b0d31fd
Skial Bainn <skial.bainn@gmail.com>
2015-02-26 14:31:25 +0000

    Change a few things after another look :/

--- src/uhx/macro/KlasImp.hx
+++ src/uhx/macro/KlasImp.hx
@@ -134,105 +134,31 @@ class KlasImp {
 			fields = def( cls, fields );
 		}
 		
-		// This is a horrible idea - I want to get rid of it. Still fun tho.
-		// -----
-		// Really sad that I have to destroy and rebuild a class just to get what I want...
-		// All callbacks handle the rename hack. @:native('orginal.package.and.Name')
-		// All retyped classes should not modify the fields further.
-		/*for (callback in reTypes) {
-			var td = callback( cls, fields );
-			
-			if (td == null) continue;
-			
-			switch( td.kind ) {
-				case TDClass(c, _, _):
-					trace( TPath( c ).toString() );
-					try {
-						Context.getType( TPath( c ).toString() );
-					} catch (e:Dynamic) {
-						POSTPONED.set( TPath( c ).toString(), td );
-						LINEAGE.set( TPath( c ).toString(), td.pack.toDotPath( td.name ) );
-						continue;
-					}
-					
-				case _:
+		for (key in RETYPE.keys()) if (cls.meta.has( key )) {
+			// Build the cache.
+			if (!RETYPE_PREVIOUS.exists( cls.pack.toDotPath( cls.name ) )) {
+				RETYPE_PREVIOUS.set( cls.pack.toDotPath( cls.name ), { cls:cls, fields:fields } );
+				
 			}
-			// Unfortuantly for this to work, all types must
-			// be in referenced by their full path. So Array<MyClass>
-			// must be Array<my.pack.to.MyClass>. This what the code
-			// below does.
-			/*for (field in td.fields) {
-				switch (field.kind) {
-					case FVar(t, e):
-						trace( t );
-						if (t != null) {
-							t = t.qualify();
-						} else if (e != null) {
-							t = Context.toComplexType( Context.typeof( e ) );
-						} else {
-							trace( field.name );
-						}
-						field.kind = FVar(t, e);
-						trace( t );
-					case FProp(g, s, t, e):
-						if (t != null) {
-							t = t.qualify();
-						} else if (e != null) {
-							t = Context.toComplexType( Context.typeof( e ) );
-						} else {
-							trace( field.name );
-						}
-						field.kind = FProp(g, s, t, e);
-						
-					case FFun(method):
-						for (arg in method.args) {
-							if (arg.type != null) {
-								arg.type = arg.type.qualify();
-							}
-						}
-						
-				}
-			}*/
-			//buildLineage( td.pack.toDotPath( td.name ) );
 			
-			/*switch (td.kind) {
-				case TDClass(s, i, b): i.remove( { name: 'Klas', pack: [], params: [] } );
-				case _:
-			}*/
-			//trace( td.printTypeDefinition() );
-			/*Compiler.exclude( cls.pack.toDotPath( cls.name ) );
-			Context.defineType( td );*/
-			//Context.getType( td.path() );
-		//}
+			// Run any pending calls to `KlasImp.retype`.
+			if (RETYPE_PENDING.exists( cls.pack.toDotPath( cls.name ) ) && RETYPE_PENDING.get( cls.pack.toDotPath( cls.name ) )) {
+				retype( cls.pack.toDotPath( cls.name ), key, cls, fields );
+				RETYPE_PENDING.set( cls.pack.toDotPath( cls.name ), false );
+				
+			}
+			
+		}
 		
 		return fields;
 	}
 	
-	/*private static var POSTPONED:StringMap<TypeDefinition> = new StringMap<TypeDefinition>();
-	private static var LINEAGE:StringMap<String> = new StringMap<String>();
-	
-	private static function buildLineage(path:String) {
-		//trace( path );
-		for (k in LINEAGE.keys() ) trace( k, LINEAGE.get( k ) );
-		for (k in POSTPONED.keys() ) trace( k, POSTPONED.get( k ) );
-		if (LINEAGE.exists( path ) && POSTPONED.exists( path )) {
-			
-			var td = POSTPONED.get( path );
-			buildLineage( td.pack.toDotPath( td.name ) );
-			
-			trace( td );
-			//Compiler.exclude( path );
-			Context.defineType( td );
-			
-		}
-	}*/
-	
-	public static function retype(key:String, ?cls:ClassType, ?fields:Array<Field>):Bool {
+	public static function retype(path:String, metadata:String, ?cls:ClassType, ?fields:Array<Field>):Bool {
 		var result = false;
 		
-		if (RETYPE.exists( key )) {
+		if (RETYPE_PREVIOUS.exists( path )) {
 			// Fetch the previous class and fields.
-			var prev = RETYPE_PREVIOUS.exists( key ) ? RETYPE_PREVIOUS.get( key ) : null;
+			var prev = RETYPE_PREVIOUS.get( path );
 			
 			// Set `cls` and `fields` only if the cache exists.
 			if (cls == null && prev != null) cls = prev.cls;
@@ -240,7 +166,7 @@ class KlasImp {
 			
 			if (cls != null && fields != null) {
 				// Pass `cls` and `fields` to the retype handler to get a `typedefinition` back.
-				var td = RETYPE.get( key )( cls, fields );
+				var td = RETYPE.get( metadata )( cls, fields );
 				
 				var nativeF = metadataFilter.bind(_, ':native');
 				
@@ -261,11 +187,15 @@ class KlasImp {
 				prev.fields = td.fields;
 				prev.cls = Context.getType( td.pack.toDotPath( td.name ) );
 				
-				RETYPE_PREVIOUS.set( key, prev );
+				RETYPE_PREVIOUS.set( path, prev );
 				
 				result = true;
 				
 			}
+			
+		} else {
+			RETYPE_PENDING.set( path, true );
+			if (cls != null && fields != null) RETYPE_PREVIOUS.set( path, { cls:cls, fields:fields } );
 			
 		}
 		
