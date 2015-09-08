c366ddd5664d56390ac09b606567e707747f28e9
Skial Bainn <skial.bainn@gmail.com>
2015-09-08 09:02:32 +0100

    Add `numListeners` to klas/Signal.hx

--- src/uhx/macro/klas/Signal.hx
+++ src/uhx/macro/klas/Signal.hx
@@ -7,7 +7,17 @@ package uhx.macro.klas;
 @:forward(keys, exists, iterator) 
 abstract Signal<T0, T1, T2>(Map<T0, RVSignal<T1, T2>>) from Map<T0, RVSignal<T1, T2>> to Map<T0, RVSignal<T1, T2>> {
 	
+	public var numListeners(get, never):Int;
+	
 	public inline function new(u:Map<T0, RVSignal<T1, T2>>) this = u;
+	
+	private inline function get_numListeners():Int {
+		var total = 0;
+		
+		for (key in this.keys()) total += this.get( key ).numListeners;
+		
+		return total;
+	}
 	
 	public inline function add(metadata:T0, callback:T1->T2->T2):RVSlot<T1, T2> {
 		var signal = null;
